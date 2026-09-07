7132969c3e0ef7d732c4c9c6a6cf9f4a84e20a96
kobayashimai <kobayashimai@localhost>
2026-09-07 11:17:34 +0900

    Java Memo Project 基本機能完成

---
+++ src/javamemoproject/ConsoleUtil.java
@@ -0,0 +1,16 @@
+package javamemoproject;
+
+public class ConsoleUtil {
+
+	public static void showDivider() {
+		System.out.println();
+		System.out.println("----------------");
+		System.out.println();
+	}
+
+	public static void showLocation(String location) {
+		showDivider();
+		System.out.println("現在地：" + location);
+		System.out.println();
+	}
+}
